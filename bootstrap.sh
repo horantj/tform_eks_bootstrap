d6af3711606bfb7bf642fa5597f5c422cca517fc
tjhoran4 <tjhoran4@gmail.com>
2018-10-20 10:08:07 -0400

    cleaned up variables in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,12 +6,26 @@
 # ARN for nginx ingress is specified in env variable AWS_CERT_ARN=
 
 #cluster name variable
-: ${TF_VAR_cluster_name="eks-cluster"}
+: ${CLUSTER_NAME="eks-cluster"}
+export TF_VAR_cluster_name=$CLUSTER_NAME
 
 #external DNS name variable
-ELB_HOST="*.gigantor.be"
-HOSTED_ZONE="Z2D252C5RIIP8I"
-ELB_ZONE="Z35SXDOTRQ7X7K"
+: ${ELB_INGRESS_DOMAIN="gigantor.be"}
+
+#external ingress host, wildcard by default
+ELB_INGRESS_HOST="*"
+ELB_HOST=$ELB_INGRESS_HOST.$ELB_INGRESS_DOMAIN
+
+#pull hosted zone from domain and validate
+ELB_DOMAIN_HOSTED_ZONE="Z2D252C5RIIP8I"
+
+#AWS ELB hosted zone to add DNS, computed
+ELB_AWS_HOSTED_ZONE="Z35SXDOTRQ7X7K"
+
+echo "cluster name: ${CLUSTER_NAME}"
+echo "ingress domain: ${ELB_INGRESS_DOMAIN}"
+echo "ingress hostname: ${ELB_HOST}"
+echo "hosted domain (${ELB_INGRESS_DOMAIN}): ${ELB_DOMAIN_HOSTED_ZONE}"
 
 #initialize tf and apply to build the cluster
 #terraform init
@@ -39,7 +53,7 @@ cat <<EOF >dns.json
                   "Name": "${ELB_HOST}",
                   "Type": "A",
                   "AliasTarget": {
-                    "HostedZoneId": "${ELB_ZONE}",
+                    "HostedZoneId": "${ELB_AWS_HOSTED_ZONE}",
                     "DNSName": ${ELB_DNS},
                     "EvaluateTargetHealth": false
                   }
@@ -50,4 +64,4 @@ cat <<EOF >dns.json
 EOF
 
 # create actual DNS entry via aws cli
-aws route53 change-resource-record-sets --hosted-zone-id ${HOSTED_ZONE} --change-batch file://dns.json
+#aws route53 change-resource-record-sets --hosted-zone-id ${ELB_DOMAIN_HOSTED_ZONE} --change-batch file://dns.json
